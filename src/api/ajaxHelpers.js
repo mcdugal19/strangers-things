4cd535530609ad6d400e9fa126a962eeab3172eb
Joel <joel.t.block@gmail.com>
2022-03-11 12:52:58 -0600

    working on ajaxHelpers

---
+++ src/api/ajaxHelpers.js
@@ -0,0 +1,30 @@
+const APIURL = "https://strangers-things.herokuapp.com/api/2202-FTB-ET-WEB-FT/";
+
+export async function fetchPosts() {
+  try {
+    const response = await fetch(`${APIURL}posts`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function registerUser(username, password) {
+  try {
+    const response = await fetch(`${APIURL}users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    throw e;
+  }
+}
