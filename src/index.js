f9d988bc042c9e71d8082c14bb853e4f1557e074
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-11 10:21:28 -0600

    First commit: boilerplate ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import {App} from './components';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <App/>
+  </Router>,
   document.getElementById('root')
 );
