efb34007c6395887a6c7033254f49a9381183d06
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-13 18:03:14 -0500

    worked on styling the post buttons with pictures. could use a little adjusting but they are there.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import edit from "./images/edit.png";
+import mail from "./images/mail.jpeg";
+import deletePost from "./images/deletePost.jpeg";
 import { fetchPosts } from "../api/ajaxHelpers";
 
 const Posts = ({ posts, setPosts }) => {
@@ -22,8 +24,11 @@ const Posts = ({ posts, setPosts }) => {
         return (
           <div className="post-card" key={post._id}>
             <h3 className="post-title">{post.title}</h3>
+            
             <h4 className="post-username">From: {post.author.username}</h4>
+            <br/>
             <p className="post-content">{post.description}</p>
+            <br/>
             <span className="post-time">
                 {console.log(post.createdAt)}
               <p className="post-created">Created On:{post.createdAt}</p>
@@ -31,9 +36,10 @@ const Posts = ({ posts, setPosts }) => {
                 <p className="post-updated">Last Updated On:{post.updatedAt}</p>
               ) : null}
             </span>
-            <button className="button" id="message">{}Message</button>
-            <button className="button" id="edit">{}Edit</button>
-            <button className="button" id="delete">{}Delete</button>
+            <br/>
+            <button className="button" id="message">{<img src={mail} width={35} height={25} alt="Mail" />}Message</button>
+            <button className="button" id="edit">{<img src={edit} width={25} alt="Edit" />}Edit</button>
+            <button className="button" id="delete">{<img src={deletePost} width={23} alt="Post Delete" />}Delete</button>
           </div>
         );
       })}
